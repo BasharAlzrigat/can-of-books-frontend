d6951e031a02d48d102a28341ff67c90663fbea3
bashar alzrigat <bashar.zureikat@gmail.com>
2022-08-10 06:53:12 +0300

    add server

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import Login from './Login'
 import Logout from './Logout'
+import About from './About'
 import './theme/Header.css'
 import Logo from './theme/logo.png'
 
@@ -13,6 +14,7 @@ class Header extends React.Component {
         <Navbar.Brand><a href = "/"><img className = 'logoHeaderImg'src = {Logo} alt = 'logo'/></a></Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         <NavItem><Link to="./User" className="nav-link">Dashboard</Link></NavItem>
+        <NavItem><Link to="./About" className="nav-link">About</Link></NavItem>
 
 <Login />
         <Logout />
